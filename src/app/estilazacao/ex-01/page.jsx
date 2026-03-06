3957c8f76f450b238e360596a116eaebaad90af6
MARIA ANTONIA HIPOLITO DA COSTA SANTOS <maria.santos3661>
2026-03-06 11:45:13 -0300

    aula organição projeto

---
+++ src/app/estilazacao/ex-01/page.jsx
@@ -0,0 +1,9 @@
+function exemplo01() {
+    return(
+        <div>
+            <h1> exemplo 1</h1>
+            <p> componente básico para renderizar tela com react</p>
+        </div>
+    )
+}
+export default exemplo01
