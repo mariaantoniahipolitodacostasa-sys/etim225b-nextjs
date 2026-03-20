0b17d50071c641f820c72637f0ee1ddb52dceba4
MARIA ANTONIA HIPOLITO DA COSTA SANTOS <maria.santos3661>
2026-03-20 13:49:36 -0300

    aula exemplo 3

---
+++ src/app/exemplos/ex-03/page.jsx
@@ -0,0 +1,19 @@
+'use client'
+
+import { useState } from "react";
+
+export default function Exemplo03() {  
+
+    const [num, setNum] = useState(100);
+
+    function handleIncrementa () {
+        setNum(num + 1);
+    }
+
+    return (
+        <>
+            <label>{`Contador: ${num}`}</label>            
+            <label onClick={() => handleIncrementa()}>+1</label>
+        </>
+    );
+}
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -6,12 +6,14 @@ function Home() {
         <div className={styles.containerhome}>
             <div className={styles.containerlistas}>
                 <h1> exemplos </h1>
-                <Link href="/exemplo/ex-01"> exemplo 1 </Link>
-                 <Link href="/exemplo/ex-02"> exemplo 2 </Link>
+                <Link href="/exemplos/ex-01"> exemplo 1 </Link>
+                 <Link href="/exemplos/ex-02"> exemplo 2 </Link> 
+                  <Link href="/exemplos/ex-03"> exemplo 3 </Link>
+
             </div>
             <div className={styles.containerlistas}>
                 <h1> atividades </h1>
-             <Link href="/atividade/atv-01"> atividade 1 </Link>
+             <Link href="/atividades/atv-01"> atividade 1 </Link>
             </div>
         </div>
     )
