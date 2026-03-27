cb4f0c3d3d713b12a3aa49ee9e5ebbf733081e61
MARIA ANTONIA HIPOLITO DA COSTA SANTOS <maria.santos3661>
2026-03-27 11:34:00 -0300

    sla

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,22 +1,24 @@
-import Link from 'next/link';
+import Image from "next/image";
+
+import Link from "next/link";
 import styles from "./page.module.css";
 
 function Home() {
-    return (
-        <div className={styles.containerhome}>
-            <div className={styles.containerlistas}>
-                <h1> exemplos </h1>
-                <Link href="/exemplos/ex-01"> exemplo 1 </Link>
-                 <Link href="/exemplos/ex-02"> exemplo 2 </Link> 
-                  <Link href="/exemplos/ex-03"> exemplo 3 </Link>
-
-            </div>
-            <div className={styles.containerlistas}>
-                <h1> atividades </h1>
-             <Link href="/atividades/atv-01"> atividade 1 </Link>
-            </div>
-        </div>
-    )
+  return (
+    <div className={styles.containerHome}>
+   <div className={styles.containerlistas}>
+    <h1>Exemplos</h1>
+    <Link href="/exemplos/ex-01">Exemplo 1</Link>
+    <Link href="/exemplos/ex-02">Exemplo 2</Link>
+     <Link href="/exemplos/ex-03">Exemplo 3</Link>
+   </div>
+   <div className={styles.containerlistas}>
+    <h1>atividades</h1>
+     <Link href="/app/atividades/atv-01">atividade 1</Link>
+      <Link href="/app/atividades/atv-02">atividade 2</Link>
+   </div>
+   </div>
+  );
 }
 
 export default Home;
